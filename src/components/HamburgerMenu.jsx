2d9e44daa1545465f04b651e7e0640fe73a3f7d1
chefthomas <thomcdixon@gmail.com>
2019-07-26 17:32:28 -0700

    feat(*): add animated hamburger menu that appears on small viewport. clicking menu opens dropdown
    HamburgerMenu.jsx component toggles dropdown menu. Appears when small viewport breaks design.

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import "../stylesheets/css/main.css";
 
 export default class HamburgerMenu extends Component {
-  state = {
-    clicked: false
+  handleClick = e => {
+    this.props.toggleDropdown();
   };
+
   render() {
+    const { showDropdownNav } = this.props;
+    console.log(showDropdownNav);
+
     return (
-      <div>
-        <div
-          className={
-            this.state.clicked ? "HamburgerMenu-clicked" : "HamburgerMenu"
-          }
-        />
+      <div className="Hamburger-container" onClick={this.handleClick}>
+        <div className={showDropdownNav ? "transparent-bar" : "top"} />
+        <div className={showDropdownNav ? "center1-rotate" : "center1"} />
+        <div className={showDropdownNav ? "center2-rotate" : "center2"} />
+        <div className={showDropdownNav ? "transparent-bar" : "bottom"} />
       </div>
     );
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,36 +5,35 @@ import "../stylesheets/css/main.css";
 import DropdownNav from "./DropdownNav";
 import NavButton from "./NavButton";
 import HamburgerMenu from "./HamburgerMenu";
+// import HamburgerMenu from "./HamburgerMenu";
 
 class Navbar extends Component {
   state = {
-    showNav: false
+    showDropDownNav: false
   };
 
   handleClick = e => {
     console.log("click burger");
   };
 
+  // toggles visibility on click
   toggleDropdown = e => {
-    this.setState({ showNav: !this.state.showNav });
+    this.setState({ showDropdownNav: !this.state.showDropdownNav });
   };
 
+  // closes on screen resize
   closeDropdown = e => {
     this.setState({ showNav: false });
   };
 
   render() {
+    const { showDropdownNav } = this.state;
     return (
       <div className="Navbar">
         <DropdownNav
-          class={this.state.showNav ? "DropdownNav" : "Dropdown-nav-hide"}
+          class={showDropdownNav ? "DropdownNav" : "Dropdown-nav-hide"}
           closeDropdown={this.closeDropdown}
         />
-        <i
-          onClick={this.toggleDropdown}
-          className="Navbar-burger fas fa-bars"
-        />
-        {/* <HamburgerMenu toggleDropdown={this.toggleDropdown}/> */}
         <div className="Navbar-links-container">
           <NavLink
             exact
@@ -60,14 +59,6 @@ class Navbar extends Component {
           >
             Book
           </NavLink>
-          {/* <NavLink
-            exact
-            className="Navbar-link"
-            activeClassName="NavLink-active"
-            to="/payment"
-          >
-            Payment
-          </NavLink> */}
           <NavLink
             exact
             className="Navbar-link"
@@ -77,6 +68,10 @@ class Navbar extends Component {
             About
           </NavLink>
         </div>
+        <HamburgerMenu
+          showDropdownNav={this.state.showDropdownNav}
+          toggleDropdown={this.toggleDropdown}
+        />
         <div className="Navbutton-container">
           <NavButton label="Log in" to="/schedule" />
           <NavButton label="Sign up" to="/schedule" />
